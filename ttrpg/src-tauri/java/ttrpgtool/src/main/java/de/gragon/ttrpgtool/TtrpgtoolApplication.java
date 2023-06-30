f8b92d1c4cb066cd5c62eb42cc2523496291609f
Tristan Nußbaum <gragonator02@gmail.com>
2023-06-30 12:48:39 +0200

    Setup Spring Backend

---
+++ ttrpg/src-tauri/java/ttrpgtool/src/main/java/de/gragon/ttrpgtool/TtrpgtoolApplication.java
@@ -0,0 +1,14 @@
+package de.gragon.ttrpgtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtrpgtoolApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+		//SpringApplication.run(TtrpgtoolApplication.class, args);
+	}
+
+}
---
+++ ttrpg/src-tauri/java/ttrpgtool/src/test/java/de/gragon/ttrpgtool/TtrpgtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gragon.ttrpgtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtrpgtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
