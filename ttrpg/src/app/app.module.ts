be61fc10e218ea303fc2547d9b055704f8b194f8
Tristan Nußbaum <gragonator02@gmail.com>
2023-07-02 00:46:56 +0200

    Added drawer aka sidenav

--- ttrpg/src/app/app.module.ts
+++ ttrpg/src/app/app.module.ts
@@ -11,13 +11,16 @@ import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from "@angular/material/divider";
 import { MatIconModule } from "@angular/material/icon";
+
 import { StartscreenComponent } from './components/startscreen/startscreen.component';
+import { DrawerComponent } from './components/drawer/drawer.component';
 
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		StartscreenComponent
+		StartscreenComponent,
+		DrawerComponent
 	],
 	imports: [
 		BrowserModule,
