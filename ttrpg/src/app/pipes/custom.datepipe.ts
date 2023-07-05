f6dc06daacb861cc09fb782a485c5fcf223a02b3
Tristan Nußbaum <gragonator02@gmail.com>
2023-07-05 12:45:58 +0200

    Renamed pipe

--- ttrpg/src/app/pipes/custom.datepipe.ts
+++ ttrpg/src/app/pipes/custom.datepipe.ts
@@ -1,14 +1,12 @@
-import { DatePipe } from '@angular/common';
-import { Pipe, PipeTransform } from '@angular/core';
-import { registerLocaleData } from '@angular/common';
+import { DatePipe, registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
+import { Pipe, PipeTransform } from '@angular/core';
 registerLocaleData(localeDe, 'de');
 
 @Pipe({
-	name: 'lastOpenedDate'
+	name: 'ttrpgDateFormat'
 })
-export class CustomDatePipe extends
-	DatePipe implements PipeTransform {
+export class CustomDatePipe extends DatePipe implements PipeTransform {
 	override transform(value: any, args?: any): any {
 		return super.transform(value, "d MMMM y", 'UTC', 'de');
 	}
