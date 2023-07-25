cb1d3cb2ad1b909805807bbb0b0b985c3d37d6d4
Tristan Nußbaum <gragonator02@gmail.com>
2023-07-25 13:44:10 +0200

    Fixed bug creating new characters everytime the new character pages is reloaded

--- ttrpg/src/app/components/main/profile/profile.component.ts
+++ ttrpg/src/app/components/main/profile/profile.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Character } from 'src/app/models/Character';
-import { emptyCharacter, newCharacter } from 'src/app/resources/constants/Characters';
+import { emptyCharacter } from 'src/app/resources/constants/Characters';
 
 import { CharacterService } from 'src/app/services/database/character.service';
 
@@ -25,38 +25,17 @@ export class ProfileComponent {
 	 * Sets the selected character by reading the id out of the url
 	 */
 	private setSelectedCharacter(): void {
-		const id = Number(this.route.snapshot.paramMap.get('id'))
+		const id = Number(this.activatedRoute.snapshot.paramMap.get('id'))
 		this.characterService.getCharacterFromDbById(id).then((character) => {
 			if (character) {
-				this.setWithExistingCharacter(character)
-			} else {
-				this.setWithNewCharacter()
+				this.characterService.setCharacter(character)
+				this.characterService.updateCharacterInDb(this.character, { lastOpened: new Date })
 			}
 		})
 	}
 
-	/**
-	 * Setter for when the character already exists
-	 * 
-	 * @param character The existing character recieved from another source (e.g. the database)
-	 */
-	private setWithExistingCharacter(character: Character): void {
-		this.characterService.setCharacter(character)
-		this.characterService.updateCharacterInDb(this.character, { lastOpened: new Date })
-	}
-
-	/**
-	 * Setter for when the character does not exist yet
-	 * Creates a new character from newCharacter template
-	 */
-	private setWithNewCharacter(): void {
-		this.characterService.setCharacter(Object.assign({}, newCharacter))
-		this.characterService.addCharacterToDb(this.character)
-			.then(() => this.characterService.updateCharacterInDb(this.character, { lastOpened: new Date }))
-	}
-
 	constructor(
-		private route: ActivatedRoute,
+		private activatedRoute: ActivatedRoute,
 		private characterService: CharacterService
 	) { }
 }
--- ttrpg/src/app/components/main/startscreen/startscreen.component.ts
+++ ttrpg/src/app/components/main/startscreen/startscreen.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'dexie';
 import { Character } from 'src/app/models/Character';
+import { newCharacter } from 'src/app/resources/constants/Characters';
 import { CharacterService } from "../../../services/database/character.service";
 
 @Component({
@@ -11,7 +13,21 @@ import { CharacterService } from "../../../services/database/character.service";
 export class StartscreenComponent {
 	public characters: Observable<Character[]> = this.characterService.getAllCharactersFromDb('lastOpened', true)
 
+	/**
+	 * Creates a new character from the newCharacter template
+	 */
+	public createNewCharacter() {
+		const character = Object.assign({}, newCharacter)
+		this.characterService.setCharacter(character)
+		this.characterService.addCharacterToDb(character)
+			.then((id) => {
+				this.characterService.updateCharacterInDb(character, { lastOpened: new Date })
+				this.router.navigate([`../profile/${id}`])
+			})
+	}
+
 	constructor(
+		private router: Router,
 		private characterService: CharacterService
 	) { }
 }
