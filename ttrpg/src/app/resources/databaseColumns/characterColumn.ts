ff16ce1c212ad521b8ed4a86aedfa3d09be49e9d
Tristan Nußbaum <tnussbaum@stud.hs-heilbronn.de>
2023-08-06 17:24:44 +0200

    Removed non functional constants

--- ttrpg/src/app/resources/databaseColumns/characterColumn.ts
+++ ttrpg/src/app/resources/databaseColumns/characterColumn.ts
@@ -1,12 +1,8 @@
-export const lastOpenedColumn: string = 'lastOpened'
-export const nameColumn: string = 'name'
-export const nicknameColumn: string = 'nickname'
-
 export const allCharacterColumns: string = concatAllColumns()
 
 function concatAllColumns(): string {
 	return '++id'
-		+ `,${lastOpenedColumn}`
-		+ `,${nameColumn}`
-		+ `,${nicknameColumn}`
+		+ ', lastOpened'
+		+ ', name'
+		+ ', nickname'
 }
