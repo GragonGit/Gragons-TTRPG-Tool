844ed5c1227d104278cc3cc844be880f953ca87e
Tristan <gragonator02@gmail.com>
2023-12-15 22:22:55 +0100

    Added home component

--- ttrpg/src/app/app-routing.module.ts
+++ ttrpg/src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/feature/home.module').then((m) => m.HomeModule),
+  },
+	{
+		path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+	}
 ];
 
 @NgModule({
