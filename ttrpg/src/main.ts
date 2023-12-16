f4fdd83143c90dff7847a9c8384dd2db8f7c0420
Tristan <gragonator02@gmail.com>
2023-12-16 02:14:02 +0100

    Removed unused import

--- ttrpg/src/main.ts
+++ ttrpg/src/main.ts
@@ -1,13 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
 })
   .catch(err => console.error(err));
