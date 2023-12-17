84fbd00c4cbbcd879b0704e56504ebb8c47004aa
Tristan <gragonator02@gmail.com>
2023-12-17 16:58:10 +0100

    Created first draft of character selection

--- ttrpg/src/app/home/feature/home.component.ts
+++ ttrpg/src/app/home/feature/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CharacterCardComponent } from '../ui/character-card/character-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CharacterCardComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
 export class HomeComponent {
-
+  characters: any[] = [1,2,3,4,5,6,7,8]
 }
