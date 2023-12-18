004dd14b62a1d36e99aebee5519c190d3555406e
Tristan <gragonator02@gmail.com>
2023-12-18 17:21:37 +0100

    Added characters table

--- ttrpg/src/app/shared/data-access/database.ts
+++ ttrpg/src/app/shared/data-access/database.ts
@@ -1,11 +1,19 @@
 import Dexie, { Table } from 'dexie';
 import { environment } from 'src/environments/environment';
 
+export interface Character {
+  id?: number
+  fileName: string
+  lastOpened?: Date
+}
+
 export class AppDB extends Dexie {
+  characters!: Table<Character, number>
 
   constructor() {
     super('ngdexieliveQuery')
     this.version(3).stores({
+      characters: '++id'
     });
     this.on('populate', () => this.populate())
   }
@@ -15,6 +23,15 @@ export class AppDB extends Dexie {
    */
   private async populate() {
     if (!environment.production) {
+      await db.characters.bulkAdd([
+        {
+          fileName: 'Sylas'
+        },
+        {
+          fileName: 'Evelynn',
+          lastOpened: new Date()
+        }
+      ])
     }
   }
 }
