976a073a30c31d5a0b45a1e77ae70ede02b2c8ce
Tristan <gragonator02@gmail.com>
2023-12-19 16:24:38 +0100

    Added ngx-translation

--- ttrpg/src/main.ts
+++ ttrpg/src/main.ts
@@ -1,9 +1,28 @@
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
 
+export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient],
+        },
+      }))]
 })
   .catch(err => console.error(err));
