f61bd49df01ae830b0305f04e278211e7087d9fd
Tristan <gragonator02@gmail.com>
2024-03-04 18:11:15 +0100

    Generated theme and language components

---
+++ ttrpg/src/app/settings/ui/settings-language/settings-language.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ttrpg-settings-language',
+  standalone: true,
+  imports: [],
+  templateUrl: './settings-language.component.html',
+  styleUrl: './settings-language.component.sass'
+})
+export class SettingsLanguageComponent {
+
+}
