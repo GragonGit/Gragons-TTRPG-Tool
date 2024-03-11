fdebbcf622d36194d8fb2a6a0eaaab716867af53
Tristan <gragonator02@gmail.com>
2024-03-11 11:42:26 +0100

    Datepipe now adjusts to selected Language

--- ttrpg/src/app/shared/pipes/custom.datepipe.ts
+++ ttrpg/src/app/shared/pipes/custom.datepipe.ts
@@ -1,13 +1,18 @@
 import { DatePipe, registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
-import { Pipe, PipeTransform } from '@angular/core';
+import localeEn from '@angular/common/locales/en';
+import { Pipe, PipeTransform, inject } from '@angular/core';
+import { LOCAL_STORAGE_KEYS } from '@data-access/localStorage';
+import { TranslateService } from '@ngx-translate/core';
 registerLocaleData(localeDe, 'de');
+registerLocaleData(localeEn, 'en');
 
 @Pipe({
   name: 'ttrpgDateFormat',
   standalone: true
 })
 export class CustomDatePipe extends DatePipe implements PipeTransform {
+  private translateService = inject(TranslateService)
   /**
    * Format a date to the TTRPG format
    * 
@@ -16,6 +21,6 @@ export class CustomDatePipe extends DatePipe implements PipeTransform {
    * @returns The formated date
    */
   public override transform(value: any): any {
-    return super.transform(value, "d MMMM y", 'UTC', 'de');
+    return super.transform(value, "d MMMM y", 'UTC', localStorage.getItem(LOCAL_STORAGE_KEYS.LANG) ?? this.translateService.getDefaultLang());
   }
 }
